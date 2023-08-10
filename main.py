2993665af9a89d39429179500df5b2c172026e5e
proviveknayan <proviveknayan@gmail.com>
2023-08-10 07:33:12 +0530

    read pdfs and setup pypdf2 get text

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import os
 import streamlit as st
+from PyPDF2 import PdfReader
+
+## get the list of PDF(s) in the root directory
+pdf_files = [f for f in os.listdir('.') if f.endswith('.pdf')]
+
+## function to get the text from the PDF(s)
+def get_pdf_text(pdf_files):
+    text = ""
+    for pdf in pdf_files:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
     st.set_page_config(page_title="Chat With Your PDF(s)", page_icon=":sunglasses:")
@@ -8,6 +22,17 @@ def main():
     
     with st.sidebar:
         st.subheader("Your Documents")
+        st.write(f'Found {len(pdf_files)} PDF file(s).')
+        st.write('- ' + '\n- '.join(pdf_files))
+        with st.spinner("Processing the PDFs"):
+
+            ## get the text from PDF(s)
+            pdf_text = get_pdf_text(pdf_files)
+            #st.write(pdf_text)
+
+            ## split the text into smaller chunks
+
+            ## create vector store for the chunks
 
 if __name__ == '__main__':
     main()
