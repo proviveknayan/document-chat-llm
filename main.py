c9a0ad5ff9ddf655471f12c06a22b25ebcd176ce
proviveknayan <proviveknayan@gmail.com>
2023-09-15 11:45:29 +0530

    add chat function. add smaller pdf.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain.vectorstores import FAISS ## to create vector store for the embed
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory ## to remember the running context of a chat
-from langchain.chains import ConversationalRetrievalChain ## to get chat histry and reply to queries
+from langchain.chains import ConversationalRetrievalChain ## to get chat history and reply to queries
 
 ## get the list of PDF(s) in the root directory
 pdf_files = [f for f in os.listdir('.') if f.endswith('.pdf')]
@@ -40,20 +40,33 @@ def get_chat_chain(vector_store):
     chat_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     return chat_chain
 
+## function to handle user question
+def handle_user_question(user_question):
+    response = st.session_state.chat_chain({'question':user_question})
+    st.session_state.chat_history = response['chat_history']
+
 def main():
+    load_dotenv()
+
     st.set_page_config(page_title="Chat With Your PDF(s)", page_icon=":sunglasses:")
 
     st.header("Chat With Your PDF(s) :sunglasses:")
-    st.text_input("Ask a question to your document(s):")
+    ## st.text_input("Ask a question to your document(s):")
+    if len(pdf_files) != 0:
+        user_question = st.text_input("Ask a question to your document(s):")
+        if user_question:
+            handle_user_question(user_question)
+    else:
+        st.write('Please add at least one PDF document to the root directory to initiate a chat.')
     
     with st.sidebar:
         st.subheader("Your Documents")
         if len(pdf_files) == 0:
-            st.write('Found 0 PDF file. Please add one to the root directory')
+            st.write('Oops!. Did not find any PDF document. Please add one to the root directory.')
         else:
             st.write(f'Found {len(pdf_files)} PDF file(s).')
             st.write('- ' + '\n- '.join(pdf_files))
-            with st.spinner("Processing the PDF(s)"):
+            with st.spinner("Processing the PDF(s) ..."):
 
                 ## get the text from PDF(s)
                 pdf_text = get_pdf_text(pdf_files)
@@ -67,7 +80,6 @@ def main():
                 vector_store = get_vector_store(text_chunks)
 
                 ## create conversation chain
-                load_dotenv()
                 st.session_state.chat_chain = get_chat_chain(vector_store)
 
 if __name__ == '__main__':
